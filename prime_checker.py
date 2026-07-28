d37f219a883c9a57128adcd355b825f28e4b1e35
Annabel Eyram Agyin <aeagyin@st.ug.edu.gh>
2026-07-28 10:29:08 +0000

    Added prime number checker

---
+++ prime_checker.py
@@ -0,0 +1,23 @@
+def is_prime(n):
+    if n < 2:
+        return False
+    
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+    
+    return True
+
+def main():
+    user_input = input("Enter a number: ")
+    number = int(user_input)
+    
+    result = is_prime(number)
+    
+    if result == True:
+        print(f"{number} is a prime number.")
+    else:
+        print(f"{number} is NOT a prime number.")
+
+if __name__ == "__main__":
+    main()
